e3cd9a35c630a655f02ae83c280d566a619f5215
KCostanzo <kstanz22@gmail.com>
2016-09-25 20:52:28 -0700

    bug fix

--- components/display.jsx
+++ components/display.jsx
@@ -109,7 +109,7 @@ module.exports = React.createClass({
 		var strNums = ['0','1','2','3','4','5','6','7','8','9'];
 
 		var rtnStart = document.getElementById('rtnStart').value;
-		// var rtnEnd = document.getElementById('rtnEnd').value;
+		var rtnEnd = document.getElementById('rtnEnd').value;
 		// console.log(rtnStart);
 		// console.log(rtnEnd);
 
